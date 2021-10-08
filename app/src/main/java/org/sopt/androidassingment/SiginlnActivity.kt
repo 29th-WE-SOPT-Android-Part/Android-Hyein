3d8b4a6f40a2fe62efac814f9888ee1fdb1cf10e
hyeinKim <sdu07024@naver.com>
2021-10-09 01:06:31 +0900

    sigin complete

---
+++ app/src/main/java/org/sopt/androidassingment/SiginlnActivity.kt
@@ -0,0 +1,39 @@
+package org.sopt.androidassingment
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import org.sopt.androidassingment.databinding.ActivityMainBinding
+
+class SiginlnActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        loginClick()
+        signClick()
+        setContentView(binding.root)
+    }
+
+    private fun loginClick(){
+        val intent = Intent(this, HomeActivity::class.java)
+        binding.btnLogin.setOnClickListener {
+            val id = binding.etId.text.toString()
+            val password = binding.etPass.text.toString()
+            if(id.isNotEmpty() && password.isNotEmpty()) {
+                Toast.makeText(this, "${id}님 환영합니다", Toast.LENGTH_SHORT).show()
+                startActivity(intent)
+            } else { Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()}
+        }
+    }
+
+    private fun signClick(){
+        val intent = Intent(this, SignUpActivity::class.java)
+        binding.btnSign.setOnClickListener { startActivity(intent) }
+    }
+}
