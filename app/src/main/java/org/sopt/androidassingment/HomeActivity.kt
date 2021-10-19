d1e4c09ea27c4abed7aee830bde5f094cf001312
hyein <hyein2072@gmail.com>
2021-10-20 00:02:57 +0900

    [ADD]fragment

--- app/src/main/java/org/sopt/androidassingment/HomeActivity.kt
+++ app/src/main/java/org/sopt/androidassingment/HomeActivity.kt
@@ -2,10 +2,32 @@ package org.sopt.androidassingment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.sopt.androidassingment.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+
+        initTransactionEvent()
+
+        setContentView(binding.root)
+    }
+
+    private fun initTransactionEvent(){
+        val followerFragment = FollowerFragment()
+        val repositoryFragment = RepositoryFragment()
+
+        supportFragmentManager.beginTransaction().add(R.id.container_home, followerFragment).commit()
+        val transaction = supportFragmentManager.beginTransaction()
+        binding.btnRepository.setOnClickListener{
+            transaction.replace(R.id.container_home, followerFragment)
+        }
+        binding.btnFollower.setOnClickListener{
+            transaction.replace(R.id.container_home, repositoryFragment)
+        }
     }
 }
